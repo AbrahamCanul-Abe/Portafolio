fce068db0c477f4bbfa25cd9e5c3d040377426c8
Abraham Canul <abraham23.acc@gmail.com>
2022-12-23 15:00:27 -0600

    Principio de responsabilidad unica, instanciando la clase de los poryectos de la carpeta servicios

---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,41 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public class RepositorioProyectos
+    {
+        public List<ProyectoDTO> ObtenerProyectos()
+        {
+            return new List<ProyectoDTO>(){
+                new ProyectoDTO
+            {
+                Titulo = "Amazon",
+                Descripcion = "E-Commerce realizado en ASP-NET Core",
+                Link = "https://amazon.com",
+                ImagenURL = "/imagenes/amazon.png"
+            },
+                new ProyectoDTO
+            {
+                Titulo = "New York Times",
+                Descripcion = "Página de noticias en React",
+                Link = "https://nytimes.com",
+                ImagenURL = "/imagenes/nyt.png"
+            },
+                new ProyectoDTO
+            {
+                Titulo = "Reddit",
+                Descripcion = "Red social para compartir en comunidades",
+                Link = "https://reddit.com",
+                ImagenURL = "/imagenes/reddit.png"
+            },
+                new ProyectoDTO
+            {
+                Titulo = "Steam",
+                Descripcion = "Tienda en línea para compra de videojuegos",
+                Link = "https://store.steampowered.com",
+                ImagenURL = "/imagenes/steam.png"
+            },
+            };
+        }
+    }
+}
