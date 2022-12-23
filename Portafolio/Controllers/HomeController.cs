93adbb1679fe431de8a5a829d0be7a8a349b0c2c
Abraham Canul <abraham23.acc@gmail.com>
2022-12-23 00:30:11 -0600

    agregando listado de proyectos

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -15,7 +15,46 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            return View(); 
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel()
+            {
+                Proyectos = proyectos
+            };
+            return View(modelo);
+        }
+
+        private List<ProyectoDTO> ObtenerProyectos()
+        {
+            return new List<ProyectoDTO>(){
+                new ProyectoDTO
+            {
+                Titulo = "Amazon",
+                Descripcion = "E-Commerce realizado en ASP-NET Core",
+                Link = "https://amazon.com",
+                ImagenURL = "/imagenes/amazon.png"
+            },
+                new ProyectoDTO
+            {
+                Titulo = "New York Times",
+                Descripcion = "Página de noticias en React",
+                Link = "https://nytimes.com",
+                ImagenURL = "/imagenes/nyt.png"
+            },
+                new ProyectoDTO
+            {
+                Titulo = "Reddit",
+                Descripcion = "Red social para compartir en comunidades",
+                Link = "https://reddit.com",
+                ImagenURL = "/imagenes/reddit.png"
+            },
+                new ProyectoDTO
+            {
+                Titulo = "Steam",
+                Descripcion = "Tienda en línea para compra de videojuegos",
+                Link = "https://store.steampowered.com",
+                ImagenURL = "/imagenes/steam.png"
+            },
+            };
         }
 
         public IActionResult Privacy()
