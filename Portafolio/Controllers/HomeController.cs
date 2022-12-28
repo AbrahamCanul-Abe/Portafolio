765f631a8caa0dca1bf7bf270814b6d12b9e19f5
Abraham Canul <abraham23.acc@gmail.com>
2022-12-27 23:32:16 -0600

    Eliminar logger y console logs

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -7,28 +7,15 @@ namespace Portafolio.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly IRepositorioProyectos repositorioProyectos;
-        private readonly IConfiguration configuration;
-
-        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos,
-            IConfiguration configuration)
+        
+        public HomeController(IRepositorioProyectos repositorioProyectos)
         {
-            _logger = logger;
             this.repositorioProyectos = repositorioProyectos;
-            this.configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            var apellido = configuration.GetValue<string>("Apellido");
-            _logger.LogTrace("Este es un mensaje de trace");
-            _logger.LogDebug("Este es un mensaje de debug");
-            _logger.LogInformation("Este es un mensaje de informacion");
-            _logger.LogWarning("Este es un mensaje de warning");
-            _logger.LogError("Este es un mensaje de error");
-            _logger.LogCritical("Este es un mensaje de critical" + apellido);
-            //_logger.LogInformation("Este es un mensaje de log");
             var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel()
             {
